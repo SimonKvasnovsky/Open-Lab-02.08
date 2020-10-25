1f1f614d7de0fa217266c7e74895862ae78d048f
Simon <simon.kvasnovsky@gmail.com>
2020-10-25 19:19:04 +0100

    A function that checks if a string is empty.

--- Open-Lab-02.08/Checker.cs
+++ Open-Lab-02.08/Checker.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json;
+using System.Xml;
 
 namespace Open_Lab_02._08
 {
     public class Checker
     {
+
         public bool IsEmpty(string str)
         {
-            throw new NotImplementedException();
+
+            if (String.IsNullOrEmpty(str))
+            {           
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+           
+           
+
         }
+
     }
 }
